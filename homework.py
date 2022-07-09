8903acb4f004e3559ea3d99c1e19dd497c07700b
SkyFlyer2 <textgame-vlad@yandex.ru>
2022-07-10 01:07:19 +0300

    initial

--- homework.py
+++ homework.py
@@ -1,18 +1,18 @@
 import logging
 import requests
-from time import time
+from time import time, sleep
 from telegram import ReplyKeyboardMarkup, Bot
 from telegram.ext import CommandHandler, Updater
 import os
+import sys
 from dotenv import load_dotenv
+from logging import StreamHandler
 
-
-from logging.handlers import StreamHandler
 
 # Здесь задана глобальная конфигурация для всех логгеров
 logging.basicConfig(
     level=logging.DEBUG,
-    filename='homework.log', 
+    filename='homework.log',
     format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
 )
 logging.basicConfig(
@@ -24,20 +24,25 @@ logger = logging.getLogger(__name__)
 # Устанавливаем уровень, с которого логи будут сохраняться в файл
 logger.setLevel(logging.INFO)
 # Указываем обработчик логов
-handler = StreamHandler()
+handler = StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
 
 
 load_dotenv()
 
 # AQAAAAAAJywxAAYckRc5OHVyEUGIrjVI49EoYug
-PRACTICUM_TOKEN = os.getenv('PRACTIKUM_TOKEN')
+PRACTIKUM_TOKEN = os.getenv('PRACTIUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
+#print(PRACTICUM_TOKEN)
+print(TELEGRAM_TOKEN)
+print(TELEGRAM_CHAT_ID)
+
+
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+HEADERS = {'Authorization': f'OAuth {PRACTIKUM_TOKEN}'}
 
 
 HOMEWORK_STATUSES = {
@@ -60,10 +65,10 @@ def get_api_answer(current_timestamp):
         # response.status_code = 500
         if response.status_code != 200:
             message = f'Ошибка в работе API код: {response.status_code}'
-            error_handler(message)
+            logging.error(message)
     except Exception as error:
         message = f'Сбой при работе API Yandex_Praktikum ошибка: {error}'
-        error_handler(message)
+        logging.error(message)
     return response.json()
 
 
@@ -72,30 +77,30 @@ def check_response(response):
         hw_list = response.get('homeworks', [])
     except IndexError as error:
         logging.error(f'Error while getting list of homeworks: {error}')
+        print('1')
     except KeyError as error:
         logging.error(f'Error while getting list of homeworks: {error}')
+        print('1')
     except Exception as error:
         logging.error(f'Error while getting list of homeworks: {error}')
+        print('3')
         print('Error while getting list of homeworks')
     else:
         return hw_list
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    #HOMEWORK_STATUSES
 
-    ...
-
-    verdict = ...
-
-    ...
+    verdict = HOMEWORK_STATUSES.get(homework_status)
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    if PRACTICUM_TOKEN is None or TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
+    if PRACTIKUM_TOKEN is None or TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
         return False
     return True
 
@@ -104,25 +109,27 @@ def main():
     """Основная логика работы бота."""
 
     bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time())
 
     check_tokens()
 
     while True:
         try:
             response = get_api_answer(current_timestamp)
+            print(response)
             homeworks = check_response(response)
 
             current_timestamp = homeworks[0].get('current_date')
-            message = parse_status(homeworks)
-            send_message(bot, message)
+            for homework in homeworks:
+                message = parse_status(homeworks)
+                send_message(bot, message)
 
-            time.sleep(RETRY_TIME)
+            sleep(RETRY_TIME)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logging.error(message)
-            time.sleep(RETRY_TIME)
+            sleep(RETRY_TIME)
         else:
             logger.DEBUG('Пока что всё работает хорошо')
 
