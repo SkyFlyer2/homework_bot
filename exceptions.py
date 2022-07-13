7c627f9d0cfdd6000d9c94eeac9a4ab4b38c0fd4
SkyFlyer2 <textgame-vlad@yandex.ru>
2022-07-13 03:55:31 +0300

    bot

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,6 @@
 class CheckResponseException(Exception):
     pass
+
+
+class APIResponseCodeException(Exception):
+    pass
--- homework.py
+++ homework.py
@@ -29,7 +29,7 @@ logger.critical("A CRITICAL message")
 
 load_dotenv()
 
-PRACTIKUM_TOKEN = os.getenv('PRACTIKUM_TOKEN')
+PRACTICUM_TOKEN = os.getenv('PRACTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('CHAT_ID')
 
@@ -40,7 +40,7 @@ print(TELEGRAM_CHAT_ID)
 
 RETRY_TIME = 60
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTIKUM_TOKEN}'}
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
 HOMEWORK_STATUSES = {
@@ -60,38 +60,34 @@ def get_api_answer(current_timestamp):
 
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        # response.status_code = 500
-        if response.status_code != HTTPStatus.OK:
-            message = f'Ошибка при запросе API: {response.status_code}'
-            logging.error(message)
     except Exception as error:
-        message = f'Сбой при работе API Yandex_Praktikum ошибка: {error}'
-        logging.error(message)
+        logging.error(f'Ошибка при запросе API: {error}')
+    if response.status_code != HTTPStatus.OK:
+        logging.error(f'Ошибка при запросе API: {response.status_code}')
+        raise HTTPStatus.HTTPStatusException('Ошибка доступа к API')
     return response.json()
 
 
 def check_response(response):
+
+#    hw_list = {}
     try:
-        hw_list = response.get('homeworks', [])
+        hw_list = response['homeworks']
     except IndexError as error:
         logging.error(f'Error while getting list of homeworks: {error}')
-        print('1')
     except KeyError as error:
         logging.error(f'Error while getting list of homeworks: {error}')
         print('1')
-    except Exception as error:
-        logging.error(f'Error while getting list of homeworks: {error}')
-        print('3')
-        print('Error while getting list of homeworks')
+    #except Exception as error:
+    #    logging.error(f'Error while getting list of homeworks: {error}')
+    #    print('Error while getting list of homeworks')
     if hw_list is None:
-        raise exceptions.CheckResponseException('Списка домашних заданий нет')
+        #raise exceptions.CheckResponseException('Списка домашних заданий нет')
         print(4)
     if not isinstance(hw_list, list):
         print(0)
-#        raise exceptions.CheckResponseException(
-#            'Ответ Api не является списком')
+        raise Exception('Ответ Api не является списком')
     if len(hw_list) == 0:
-        print(5)
         raise exceptions.CheckResponseException(
             'Домашнего задания нет за данный промежуток времени')
 
@@ -100,33 +96,46 @@ def check_response(response):
 
 
 def parse_status(homework):
-    homework_name = homework[0].get('homework_name')
-    homework_status = homework[0].get('status')
+    try:
+        homework_name = homework[0].get('homework_name')
+        homework_status = homework[0].get('status')
+        print(homework_status)
+    except KeyError as error:
+        logging.error(f'Error while getting list of homeworks: {error}')
+#        raise KeyError('')
+    if homework_status is None:
+        logging.error(f'Error while getting list of homeworks: {error}')
+#        raise KeyError('Ошибка, пустое значение status')
+    if homework_name is None:
+        logging.error('Ошибка, пустое значение homework_name')
+        raise KeyError
 
     verdict = HOMEWORK_STATUSES.get(homework_status)
+    if verdict is None:
+        logging.info('Статус домашней работы неизвестен')
+        raise KeyError('Ошибка, неизвестное значение status')
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    if PRACTIKUM_TOKEN is None or TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
+    if PRACTICUM_TOKEN is None or TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
         return False
     return True
 
 
 def main():
     """Основная логика работы бота."""
-
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time()) - 1209600
     previous_status = None
 
-    check_tokens()
+    if not check_tokens():
+        exit()
 
     while True:
         try:
             response = get_api_answer(current_timestamp)
-            print(response)
             homeworks = check_response(response)
             current_timestamp = response.get('current_date', [])
             #homeworks[0].get('current_date')
@@ -138,11 +147,10 @@ def main():
             else:
                 sleep(RETRY_TIME)
         except exceptions.CheckResponseException as error:
-            message = f'Обновление статуса: {error}'
-            logging.error(message)
+            logging.info(f'Обновление статуса: {error}')
+            sleep(RETRY_TIME)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logging.error(message)
+            logging.error(f'Сбой в работе программы: {error}')
             sleep(RETRY_TIME)
         else:
             logger.debug('Работа программы без замечаний')
